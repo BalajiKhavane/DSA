25db179b3cd354c8b5d4994e63d48ede97d57d5d
BalajiKhavane <balajikhavane1041@gmail.com>
2024-09-08 13:32:47 +0530

    Typecasting programs added

---
+++ explecitetypecasting.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n=10;
+    float num=20.24;
+    float sum=num + n;
+    cout<<"Sum :"<<int(sum);
+
+    return 0;
+}
---
+++ implecitetypecasting.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=10,sum;
+    double num=102.39;
+    sum=n+num;
+    cout<<"The sum is :"<<sum;
+
+    return 0;
+}
