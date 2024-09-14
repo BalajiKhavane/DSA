efb8dba3f05eaa65e6f66960c60f5b7ea959de80
BalajiKhavane <balajikhavane1041@gmail.com>
2024-09-14 23:59:59 +0530

    Fibonacci series added

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -5,16 +5,19 @@ void fibonacci(int n){
     a[0]=0;
     a[1]=1;
     a[2]=1;
-    for(i=3;i<n;i++){
+    for(i=3;i<=n;i++){
         a[i]=a[i-1]+a[i-2];
     }
     cout<<"Fibonacci Series :"<<endl;
-    for(i=0;i<n;i++){
+    for(i=0;i<=n;i++){
         cout<<a[i]<<"\t";
     }
     
 }
 int main(){
-    fibonacci(8);
+    int n;
+    cout<<"Enter a number :"<<endl;
+    cin>>n;
+    fibonacci(n);
     return 0;
 }
