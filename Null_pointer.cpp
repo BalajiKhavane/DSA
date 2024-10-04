79680d478d3bdeec7fbcdc5bab62b276d773e3a0
BalajiKhavane <balajikhavane1041@gmail.com>
2024-10-04 22:34:14 +0530

    Null pointer

---
+++ Null_pointer.cpp
@@ -0,0 +1,12 @@
+//      A pointer that doesn't point to any location.
+
+#include <iostream>
+using namespace std;
+int main(){
+    int* ptr= NULL;
+    cout<< ptr <<endl;
+    
+    return 0; 
+}
+
+
