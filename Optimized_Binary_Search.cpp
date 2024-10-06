37516c8a41fb4e1ff9b29fc4244064661f87b99c
BalajiKhavane <balajikhavane1041@gmail.com>
2024-10-06 22:23:03 +0530

    Optimized Binary Search

---
+++ Optimized_Binary_Search.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int binarySearch(vector<int>& vec,int key){
+    int size=vec.size();
+    int i=0,j=size-1,ans=-1;
+    
+    while(i<=j){
+        int mid = i + (j-i)/2;
+        // int mid= (i+j)/2;
+        if(vec[mid]<key){
+            i=mid+1;
+        }else if(vec[mid]>key){
+            j=mid-1;
+        }else{
+            ans=mid;
+            break;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    int low=0,n,key;
+    vector <int> vec;
+    cout<<"Enter the number of elements :"<<endl;
+    cin>>n;
+    cout<<"Enter the element :"<<endl;
+    for(int i=0;i<n;i++){
+        int ele;
+        cin>>ele;
+        vec.push_back(ele);
+    }
+    cout<<"Enter the key element :"<<endl;
+    cin>>key;
+    int result = binarySearch(vec,key);
+    if(result != -1){
+        cout<<"Element found at "<<result+1<<" location"<<endl;
+    }else{
+        cout<<"Element is not found"<<endl;
+    }
+
+    return 0;
+}
