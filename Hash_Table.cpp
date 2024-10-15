059075c54d73b345f203d7cd19a6ae221e03cec9
BalajiKhavane <balajikhavane1041@gmail.com>
2024-10-15 20:14:35 +0530

    Hash Table Basic

---
+++ Hash_Table.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+using namespace std;
+int main(){
+    unordered_map<string,int> Hashmap;      //defination of unordered_map
+
+    //Insertion
+    Hashmap["Balaji"]=98;
+    Hashmap["Shivtej"]=90;
+    Hashmap["Chinmay"]=95;
+
+    Hashmap.insert(pair<string,int>("Vivek",64));
+    Hashmap.insert(make_pair("Keshav",69));
+    Hashmap.insert(make_pair("Ravi",99));
+
+    //Deletion
+    Hashmap.erase("Keshav");
+    Hashmap.erase("Ravi");
+
+    //Printing Hashmap 
+    for(auto i: Hashmap){
+        cout<<i.first<<" "<<i.second<<endl;     //first -> keys     second -> values
+    }
+
+    return 0;
+}
