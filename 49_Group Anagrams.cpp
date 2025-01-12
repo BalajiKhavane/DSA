99eb32365a65c99e1ba03d947f266be91aa96eb5
BalajiKhavane <balajikhavane1041@gmail.com>
2025-01-12 23:59:33 +0530

    Leetcode 49

---
+++ 49_Group Anagrams.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+vector<vector<string>> groupAnagrams(vector<string>& strs){
+    unordered_map<string,vector<string>> umap;
+    string temp;
+    for(int i=0;i<strs.size();i++){
+        temp = strs[i];
+        sort(strs[i].begin(),strs[i].end());
+        umap[strs[i]].push_back(temp);
+    }
+
+    vector<vector<string>> ans;
+    for(auto i : umap){
+        ans.push_back(i.second);
+    }
+    return ans;
+}
+
+int main(){
+    vector<string> strs{"eat","tea","tan","ate","nat","bat"};
+    vector<vector<string>> result;
+    result = groupAnagrams(strs);
+    for(auto i : result){
+        for(auto j : i){
+            cout<<i<<"\t";
+        }cout<<endl;
+    }cout<<endl;
+
+    return 0;
+}
