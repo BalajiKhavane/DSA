a806a2db8d448ee496924bac655b477a4bdf7772
BalajiKhavane <balajikhavane1041@gmail.com>
2025-02-25 22:35:17 +0530

    Leetcode 1545

--- 1545_Find Kth Bit in Nth Binary String.cpp
+++ 1545_Find Kth Bit in Nth Binary String.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <math.h>
 using namespace std;
+char helper(int len, int k){
+    if(len == 1){
+        return '0';
+    }
+    int half = len/2;
+    int mid = half+1;
+    if(k == mid){
+        return '1';
+    }else if(k < mid){
+        return helper(half,k);
+    }else{
+        char ans =  helper(half,len+1-k);
+        return (ans == '0')? '1':'0';
+    }
+}
 char findKthBit(int n, int k) {
-    vector<string> s;
-    s.push_back("0");
-    s.push_back("011");
-    s.push_back("0111001");
-    s.push_back("011100110110001");
-    return (s[n-1][k-1]);
+    int len = pow(2,n)-1;
+    return helper(len,k);
 }
 int main(){
     int n,k;
