b6acf77aa8a060dcbaa9ab2edd276c9ae5840975
BalajiKhavane <balajikhavane1041@gmail.com>
2025-03-13 20:02:20 +0530

    Efficient Leetcode 2529

--- 2529_Maximum Count of Positive Integer and Negative Integer.cpp
+++ 2529_Maximum Count of Positive Integer and Negative Integer.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>  // For lower_bound
+
 using namespace std;
+
 int maximumCount(vector<int>& nums) {
-    int pos = 0, neg = 0;
-    for(auto i : nums){
-        if(i < 0){
-            neg++;
-        }else if(i > 0){
-            pos++;
-        }else{
-            continue;
-        }
-    }
-    return max(pos,neg);
+    int n = nums.size();
+    
+    // Find the first non-negative number index
+    int negCount = lower_bound(nums.begin(), nums.end(), 0) - nums.begin();
+    
+    // Find the first positive number index
+    int posCount = nums.end() - upper_bound(nums.begin(), nums.end(), 0);
+
+    return max(negCount, posCount);
 }
-int main(){
-    vector<int> nums = {-2,-1,-1,1,2,3};
-    cout<<maximumCount(nums)<<endl;
-    
-    nums = {-3,-2,-1,0,0,1,2};
-    cout<<maximumCount(nums)<<endl;
-    
-    nums = {5,20,66,1314};
-    cout<<maximumCount(nums)<<endl;
+
+int main() {
+    vector<int> nums = {-2, -1, -1, 1, 2, 3};
+    cout << maximumCount(nums) << endl;
+
+    nums = {-3, -2, -1, 0, 0, 1, 2};
+    cout << maximumCount(nums) << endl;
+
+    nums = {5, 20, 66, 1314};
+    cout << maximumCount(nums) << endl;
 
     return 0;
 }
