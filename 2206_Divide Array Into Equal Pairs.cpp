10614ef42005908e8623ebb884b2d8c9c98588ef
BalajiKhavane <balajikhavane1041@gmail.com>
2025-03-18 00:09:56 +0530

    Modified Leetcode 2206

--- 2206_Divide Array Into Equal Pairs.cpp
+++ 2206_Divide Array Into Equal Pairs.cpp
@@ -5,21 +5,19 @@ using namespace std;
 
 bool divideArray(vector<int>& nums) {
     unordered_map<int, int> freq;
-    for (int num : nums) {
-        freq[num]++;
+    for (int i : nums) {
+        freq[i]++;
     }
-    for (auto& [key, count] : freq) {
-        if (count % 2 != 0) return false;
+    for (auto it : freq) {
+        if (it.second % 2 != 0) return false;
     }
     return true;
 }
-
 int main() {
     vector<int> nums1 = {3, 2, 3, 2, 2, 2};
-    vector<int> nums2 = {1, 2, 3, 4};
-
-    cout << (divideArray(nums1) ? "true" : "false") << endl; // Output: true
-    cout << (divideArray(nums2) ? "true" : "false") << endl; // Output: false
+    cout<<divideArray(nums1)<<endl;
+    nums1 = {1, 2, 3, 4};
+    cout<<divideArray(nums1)<<endl;
 
     return 0;
 }
