4f5d3fb755fde8bc70d263b89590990599e920c6
BalajiKhavane <balajikhavane1041@gmail.com>
2025-03-20 22:19:03 +0530

    Leetcode 2032

--- 2032_Two Out of Three.cpp
+++ 2032_Two Out of Three.cpp
@@ -1,28 +1,37 @@
 #include <iostream>
 #include <vector>
-#include <unordered_set>
+#include <unordered_map>
+#include <set>
 using namespace std;
 vector<int> twoOutOfThree(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3) {
-    unordered_set<int> uset;
-    vector<int> ans;
-    for(auto i : nums2) uset.insert(i);
-    for(auto i : nums3) uset.insert(i);
-    for(auto i : nums1){
-        if(uset.find(i) != uset.end()){
-            ans.push_back(i);
+    unordered_map<int,int> ans;
+    set<int> set1,set2,set3;
+
+    for(auto i : nums1) set1.insert(i);
+    for(auto i : nums2) set2.insert(i);
+    for(auto i : nums3) set3.insert(i);
+
+    for(auto i : set1) ans[i]++;
+    for(auto i : set2) ans[i]++;
+    for(auto i : set3) ans[i]++;
+
+    nums1.clear();
+    for(auto i : ans){
+        if(i.second >= 2){
+            nums1.push_back(i.first);
         }
     }
-    return ans;
+    return nums1;
 }
-
 int main(){
-    vector<int> num1,num2,num3,ans;
+    vector<int> num1,num2,num3,result;
     num1 = {1,1,3,2};
     num2 = {2,3};
     num3 = {3};
-    twoOutOfThree(num1,num2,num3);
-    for(auto i : ans){
+    result = twoOutOfThree(num1,num2,num3);
+    for(auto i : result){
         cout<<i<<"\t";
     }cout<<endl;
+    
     return 0;
 }
