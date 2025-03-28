fff7586832d7b94933f9ca22ebaeddfbc4321c07
BalajiKhavane <balajikhavane1041@gmail.com>
2025-03-28 23:19:03 +0530

    Updated 3456

--- 3456_Find Special Substring of Length K.cpp
+++ 3456_Find Special Substring of Length K.cpp
@@ -2,14 +2,14 @@
 #include <string>
 using namespace std;
 bool hasSpecialSubstring(string s, int k) {
-    int count;
-    for(int i=0;i<s.size()-1;i++){
-        count = 1;
-        while(i<s.size()-1 && s[i]==s[i+1]){
+    int n = s.size();
+    for (int i = 0; i < n; i++) {
+        int count = 1;
+        while (i + 1 < n && s[i] == s[i + 1]) {
             count++;
             i++;
         }
-        if(count == k){
+        if (count == k){
             return true;
         }
     }
