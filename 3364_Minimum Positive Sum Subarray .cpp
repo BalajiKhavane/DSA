3e9e12c4191e470322b63df1c80bf5a126980b34
BalajiKhavane <balajikhavane1041@gmail.com>
2025-03-31 23:13:57 +0530

    Leetcode 3364

--- 3364_Minimum Positive Sum Subarray .cpp
+++ 3364_Minimum Positive Sum Subarray .cpp
@@ -3,27 +3,26 @@
 #include <climits>
 using namespace std;
 int minimumSumSubarray(vector<int>& nums, int l, int r) {
-    int l_sum = 0, r_sum = 0;
-    
-    // Calculate initial l_sum and r_sum
-    for(int i = 0; i < l; i++) l_sum += nums[i];
-    for(int i = 0; i < r; i++) r_sum += nums[i];
+    int n = nums.size();
+    int min_sum = INT_MAX;
+    vector<int> prefix(n + 1, 0);
 
-    int min_lsum = l_sum, min_rsum = r_sum;
-
-    // Sliding window for minimum sum subarray of length l
-    for(int i = l; i < nums.size(); i++){
-        l_sum += nums[i] - nums[i - l];
-        min_lsum = min(l_sum, min_lsum);
+    // Compute prefix sum
+    for (int i = 0; i < n; i++) {
+        prefix[i + 1] = prefix[i] + nums[i];
     }
 
-    // Sliding window for minimum sum subarray of length r
-    for(int i = r; i < nums.size(); i++){
-        r_sum += nums[i] - nums[i - r];
-        min_rsum = min(r_sum, min_rsum);
+    // Sliding window for different lengths
+    for (int size = l; size <= r; size++) {
+        for (int i = 0; i + size <= n; i++) {
+            int subarray_sum = prefix[i + size] - prefix[i]; // Sum of subarray nums[i:i+size]
+            if (subarray_sum > 0) {
+                min_sum = min(min_sum, subarray_sum);
+            }
+        }
     }
 
-    return min(min_lsum, min_rsum);
+    return (min_sum == INT_MAX) ? -1 : min_sum;
 }
 // Example usage
 int main() {
