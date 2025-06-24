c3e50d9676ae9ef712ee3138bc15278f7a921d4a
BalajiKhavane <balajikhavane1041@gmail.com>
2025-06-24 23:05:01 +0530

    Leetcode 206

---
+++ 206_Reverse Linked List.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *prev = NULL;
+        ListNode *curr = head;
+
+        while(curr != NULL){
+            ListNode *next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+};
+
+// Function to print the linked list
+void printList(ListNode* head) {
+    while (head != NULL) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+// Function to create linked list from an array
+ListNode* createList(int arr[], int n) {
+    if (n == 0) return NULL;
+    ListNode* head = new ListNode(arr[0]);
+    ListNode* current = head;
+    for (int i = 1; i < n; i++) {
+        current->next = new ListNode(arr[i]);
+        current = current->next;
+    }
+    return head;
+}
+
+int main() {
+    Solution solution;
+
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    ListNode* head = createList(arr, n);
+
+    cout << "Original List: ";
+    printList(head);
+
+    head = solution.reverseList(head);
+
+    cout << "Reversed List: ";
+    printList(head);
+
+    return 0;
+}
