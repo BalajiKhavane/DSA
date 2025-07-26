47863ff0045ee47e628892a1e68b5188fad8a306
BalajiKhavane <balajikhavane1041@gmail.com>
2025-07-26 12:37:42 +0530

    Leetcode 3 Modification

--- 3_Longest Substring Without Repeating Characters.cpp
+++ 3_Longest Substring Without Repeating Characters.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
-#include <set>
+#include <unordered_map>
 using namespace std;
 int lengthOfLongestSubstring(string s){
-    set<int> set;
-    int j=0,count=0;
-    for(int i=0;i<s.size();i++){
-        while(set.find(s[i]) != set.end()){
-            set.erase(s[j]);
-            j++;
+    int left = 0, max_cnt = 0;
+    unordered_map<char,int> umap;
+    for(int right = 0;right < s.size();right++){
+        if(umap.find(s[right]) != umap.end()){
+            left = umap[s[right]]+1;
+            umap[s[right]] = right;
+        }else{
+            umap[s[right]] = right;
+            max_cnt = max(max_cnt, right-left+1);
         }
-        set.insert(s[i]);
-        count= max(count,i-j+1);
-    }           
-    return count;         
+    }       
+    return max_cnt;
 }                              
 int main(){
     string s;
